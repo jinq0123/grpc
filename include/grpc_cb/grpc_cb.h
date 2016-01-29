287521b57364e5c80c50ddeb5d82ebc2f29cf75a
Jin Qing <jinq0123@163.com>
2016-01-29 11:14:37 +0800

    Change grpc_cb project.

---
+++ include/grpc_cb/grpc_cb.h
@@ -0,0 +1,11 @@
+// Licensed under the Apache License, Version 2.0.
+// Author: Jin Qing (http://blog.csdn.net/jq0123)
+
+#ifndef GRPC_CB_GRPC_CB_H
+#define GRPC_CB_GRPC_CB_H
+
+#include <grpc/grpc.h>
+
+#include <grpc_cb/channel.h>
+
+#endif  // GRPC_CB_GRPC_CB_H
