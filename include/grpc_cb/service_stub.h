fb48be3dc950656a584f9a21d1df2e24102878b8
Jin Qing <jinq0123@163.com>
2016-02-07 13:24:42 +0800

    Add default error callback.

--- include/grpc_cb/service_stub.h
+++ include/grpc_cb/service_stub.h
@@ -7,6 +7,7 @@
 #include <cassert>
 
 #include <grpc_cb/channel_ptr.h>
+#include <grpc_cb/error_callback.h>  // for ErrorCallback
 
 namespace grpc_cb {
 
@@ -14,16 +15,40 @@ namespace grpc_cb {
 class ServiceStub {
  public:
   inline explicit ServiceStub(const ChannelPtr& channel)
-    : channel_(channel) {  // copy
+      : channel_(channel)  // copy shared_ptr
+      , error_callback_(default_error_callback_) {
     assert(channel);
+    assert(error_callback_);
   }
   inline virtual ~ServiceStub() {}
 
  public:
   inline ChannelPtr GetChannelPtr() const { return channel_; }
+  inline const ErrorCallback& GetErrorCallback() const {
+    return error_callback_;
+  }
+  inline void SetErrorCallback(const ErrorCallback& cb) {
+    assert(cb);
+    error_callback_ = cb;
+  }
+
+ public:
+  static inline ErrorCallback& GetDefaultErrorCallback() {
+    return default_error_callback_;
+  }
+  static inline void SetDefaultErrorCallback(const ErrorCallback cb) {
+    assert(cb);
+    default_error_callback_ = cb;
+  }
+  // Default ignore error.
+  static void IgnoreError(const Status& error_status) {};
 
  protected:
   ChannelPtr channel_;
+  ErrorCallback error_callback_;
+
+ protected:
+  static ErrorCallback default_error_callback_;
 };
 
 }  // namespace grpc_cb
