16920152d17f5e74ca7d1ed9d9eb3fdbd425ea8d
Jin Qing <jinq0123@163.com>
2016-06-11 15:13:55 +0800

    Add ServerReaderCqTag.

--- include/grpc_cb/impl/server/server_reader_cqtag.h
+++ include/grpc_cb/impl/server/server_reader_cqtag.h
@@ -1,8 +1,8 @@
 // Licensed under the Apache License, Version 2.0.
 // Author: Jin Qing (http://blog.csdn.net/jq0123)
 
-#ifndef GRPC_CB_IMPL_SERVER_SERVER_READER_INIT_CQTAG_H
-#define GRPC_CB_IMPL_SERVER_SERVER_READER_INIT_CQTAG_H
+#ifndef GRPC_CB_IMPL_SERVER_SERVER_READER_CQTAG_H
+#define GRPC_CB_IMPL_SERVER_SERVER_READER_CQTAG_H
 
 #include <grpc/support/port_platform.h>    // for GRPC_MUST_USE_RESULT
 
@@ -15,7 +15,7 @@
 namespace grpc_cb {
 
 template <class Request, class Response>
-class ServerReaderInitCqTag GRPC_FINAL : public CallCqTag {
+class ServerReaderCqTag GRPC_FINAL : public CallCqTag {
  public:
   using Replier = ServerReplier<Response>;
   using MsgCallback = std::function<void (const Request&, const Replier&)>;
@@ -27,49 +27,58 @@ class ServerReaderInitCqTag GRPC_FINAL : public CallCqTag {
   using CallbacksSptr = std::shared_ptr<Callbacks>;
 
  public:
-  inline ServerReaderInitCqTag(const CallSptr& call_sptr,
-      const MsgCallback& on_msg, const EndCallback& on_end);
+  inline ServerReaderCqTag(const CallSptr& call_sptr,
+      const CallbacksSptr& cbs_sptr);
   inline Status Start() GRPC_MUST_USE_RESULT;
 
  public:
   inline void DoComplete(bool success) GRPC_OVERRIDE;
 
  private:
-  CodSendInitMd cod_send_init_md_;
-  // Already got init_md and request, so no CodRecvInitMd.
+  CodRecvMsg cod_recv_msg_;
   CallbacksSptr cbs_sptr_;
-};  // class ServerReaderInitCqTag
+};  // class ServerReaderCqTag
 
 template <class Request, class Response>
-ServerReaderInitCqTag<Request, Response>::ServerReaderInitCqTag(
-    const CallSptr& call_sptr,
-    const MsgCallback& on_msg,
-    const EndCallback& on_end)
-    : CallCqTag(call_sptr), cbs_sptr_(new Callbacks{on_msg, on_end}) {
+ServerReaderCqTag<Request, Response>::ServerReaderCqTag(
+    const CallSptr& call_sptr, const CallbacksSptr cbs_sptr)
+    : CallCqTag(call_sptr), cbs_sptr_(cbs_sptr) {
   assert(call_sptr);
-  assert(on_msg);
-  assert(on_end);
+  assert(cbs_sptr);
+  assert(cbs_sptr->on_msg);
+  assert(cbs_sptr->on_end);
 }
 
 template <class Request, class Response>
-Status ServerReaderInitCqTag<Request, Response>::Start() {
+Status ServerReaderCqTag<Request, Response>::Start() {
   CallOperations ops;
-  // Todo: Fill send_init_md_array_ -> FillMetadataVector()
-  ops.SendInitMd(cod_send_init_md_);
+  ops.RecvMsg(cod_recv_msg_);
   return GetCallSptr()->StartBatch(ops, this);
 }
 
 template <class Request, class Response>
-void ServerReaderInitCqTag<Request, Response>::DoComplete(bool success) {
+void ServerReaderCqTag<Request, Response>::DoComplete(bool success) {
   assert(success);
   const CallSptr& call_sptr = GetCallSptr();
+  Response response;
+  Status status = cod_recv_msg_.GetResultMsg(
+      response, call_sptr->GetMaxMsgSize());
+  Replier replier(call_sptr);
+  if (!status.ok()) {
+      replier.ReplyError(status);  // Todo: reply only once
+      return;
+  }
+  // XXX reading close?
+
+  cbs_sptr->on_msg(replier);
+
   auto* tag = new ServerReaderCqTag(call_sptr, cbs_sptr_);
-  ::grpc_cb::Status status = tag->Start();
+  status = tag->Start();
   if (status.ok()) return;
   delete tag;
-  Replier(call_sptr).ReplyError(status);
+  replier.ReplyError(status);
 }
 
 };  // namespace grpc_cb
 
-#endif  // GRPC_CB_IMPL_SERVER_SERVER_READER_INIT_CQTAG_H
+#endif  // GRPC_CB_IMPL_SERVER_SERVER_READER_CQTAG_H
