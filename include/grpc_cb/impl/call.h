baa8527b6df1723d1f0ea48172cc8bbbf5c7c6c1
Jin Qing <jinq0123@163.com>
2016-06-11 19:32:36 +0800

    Inline Call member functions and change StartBatch() to return bool.

--- include/grpc_cb/impl/call.h
+++ include/grpc_cb/impl/call.h
@@ -4,30 +4,28 @@
 #ifndef GRPC_CB_IMPL_CALL_H
 #define GRPC_CB_IMPL_CALL_H
 
+#include <cassert>
+
+#include <grpc/grpc.h>
 #include <grpc/support/port_platform.h>  // for GRPC_MUST_USE_RESULT
 
 #include <grpc_cb/impl/call_sptr.h>
 #include <grpc_cb/support/config.h>         // for GRPC_FINAL
-#include <grpc_cb/support/protobuf_fwd.h>   // for Message
+#include <grpc_cb/impl/call_operations.h>  // for CallOperations
 
-struct grpc_byte_buffer;
 struct grpc_call;
 
 namespace grpc_cb {
-
-class Status;
-class CallOperations;
 
 // Straightforward wrapping of the C call object.
 // Differ from grpc++:
 //  grpc_call is owned by Call instead of ClientContext/ServerContext.
 class Call GRPC_FINAL {
  public:
-  explicit Call(grpc_call* c_call);  // own
-  ~Call();
+  inline explicit Call(grpc_call* c_call);  // own
 
  public:
-  Status StartBatch(const CallOperations& ops, void* tag) GRPC_MUST_USE_RESULT;
+  inline bool StartBatch(const CallOperations& ops, void* tag) GRPC_MUST_USE_RESULT;
 
   inline int GetMaxMsgSize() const { return max_msg_size_; }
   inline void SetMaxMsgSize(int size) { max_msg_size_ = size; }
@@ -44,8 +42,21 @@ class Call GRPC_FINAL {
   int max_msg_size_;  // XXX necessary? Move to Channel?
 
  private:
-  static int default_max_msg_size_;  // for all call
+  static int default_max_msg_size_;  // = -1;  // for all call
 };
+
+// Owns c_call.
+Call::Call(grpc_call* c_call) :
+    c_call_uptr_(c_call, grpc_call_destroy),
+    max_msg_size_(default_max_msg_size_) {
+  assert(c_call);
+}
+
+bool Call::StartBatch(const CallOperations& ops, void* tag) {
+  grpc_call_error result = grpc_call_start_batch(
+    c_call(), ops.GetOps(), ops.GetOpsNum(), tag, nullptr);
+  return GRPC_CALL_OK == result;
+}
 
 }  // namespace grpc_cb
 
--- src/cpp_cb/common/call.cc
+++ src/cpp_cb/common/call.cc
@@ -3,44 +3,8 @@
 
 #include <grpc_cb/impl/call.h>
 
-#include <cassert>
-
-#include <grpc/grpc.h>
-#include <grpc_cb/impl/call_operations.h>  // for CallOperations
-#include <grpc_cb/impl/proto_utils.h>      // for DeserializeProto()
-#include <grpc_cb/status.h>                // for Status
-
 namespace grpc_cb {
 
 int Call::default_max_msg_size_ = -1;
-
-// Owns c_call.
-Call::Call(grpc_call* c_call) :
-    c_call_uptr_(c_call, grpc_call_destroy),
-    max_msg_size_(default_max_msg_size_) {
-  assert(c_call);
-}
-
-Call::~Call() {
-}
-
-// Todo: return bool...
-Status Call::StartBatch(const CallOperations& ops, void* tag) {
-  grpc_call_error result = grpc_call_start_batch(
-    c_call(), ops.GetOps(), ops.GetOpsNum(), tag, nullptr);
-  if (GRPC_CALL_OK == result) {
-    return Status::OK;
-  }
-  return Status::InternalError("grpc_call_start_batch() failed");
-}
-
-// XXX: Move out. Call is only wrap of grpc_call.
-//Status Call::GetResponse(::google::protobuf::Message* response) const {
-//  assert(response);
-//  // TODO: check status first...
-//  return DeserializeProto(recv_buf_, response, max_msg_size_);
-//}
-
-// Todo: Inline these.
 
 }  // namespace grpc_cb
